371ef8f586d66f712eace77822368f4c7e638419
simranthakkar96 <simranthakkar96@gmail.com>
2020-08-25 21:20:49 -0400

    Initial Commit

---
+++ SunriseApp WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  SunriseApp WatchKit Extension
+//
+//  Created by Simran Thakkar on 2020-08-25.
+//  Copyright © 2020 Simran Thakkar. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ SunriseApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SunriseApp
+//
+//  Created by Simran Thakkar on 2020-08-25.
+//  Copyright © 2020 Simran Thakkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
